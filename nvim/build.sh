0380f296b597dfd5da32369b00d9d1a6185f0246
takei0107 <takei0107.work@gmail.com>
2023-08-01 21:35:06 +0900

    add: nvim build script

---
+++ nvim/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+set -u
+
+# use tmp directory
+tmp_dir=/tmp/nvim-build/$(date "+%Y%m%d%H%M%S")
+echo "tmp directory: $tmp_dir"
+mkdir -p "$tmp_dir"
+
+# git clone
+git clone --branch master --single-branc --depth 1 https://github.com/neovim/neovim.git "$tmp_dir"
+cd "$tmp_dir"
+
+# build
+make distclean
+make deps
+make CMAKE_BUILD_TYPE=RelWithDebInfo
+make install
+
